5886039cfb0b5033fdbf7a683e2dfeb9e7535610
Adrian Sims <adriansims62781@gmail.com>
2025-04-15 15:33:23 +0200

    First commit

---
+++ MonSwt/CliProgram.cs
@@ -0,0 +1,70 @@
+﻿using System.Diagnostics;
+using System.IO.Pipes;
+
+namespace MonSwtCli;
+
+class CliProgram
+{
+    const string PipeName = "MonSwt.Pipe";
+    const string ExeName = "MonSwt.exe";
+    private static void Main(string[] args)
+    {
+        try
+        {
+            Console.WriteLine("Hello");
+            EnsureExeRunning();
+            using (var client = new NamedPipeClientStream(".", "MyMonitorPipe", PipeDirection.InOut))
+            {
+                try
+                {
+                    // Wait up to a second for connection:
+                    client.Connect(1000);
+
+                    using (var writer = new StreamWriter(client) { AutoFlush = true })
+                    using (var reader = new StreamReader(client))
+                    {
+                        writer.WriteLine("basic poll");
+                        string response = reader.ReadLine();
+                        Console.WriteLine(response);
+                    }
+                }
+                catch (TimeoutException)
+                {
+                    Console.WriteLine("Could not connect to GUI process.");
+                }
+            }
+
+        }
+        catch (Exception ex)
+        {
+            var fullname = System.Reflection.Assembly.GetEntryAssembly().Location;
+            var progname = Path.GetFileNameWithoutExtension(fullname);
+            Console.Error.WriteLine($"{progname} Error: {ex.Message}");
+        }
+    }
+
+    private static void EnsureExeRunning()
+    {
+        Console.WriteLine("Ensure");
+        var runningP = Process.GetProcessesByName(Path.GetFileNameWithoutExtension(ExeName));
+        bool isRunning = Process.GetProcessesByName(Path.GetFileNameWithoutExtension(ExeName)).Any();
+        if (!isRunning)
+        {
+            try
+            {
+                Console.WriteLine($"Starting process...");
+                var result = Process.Start(ExeName);
+                Console.WriteLine($"Process should be running...{result.Id}");
+
+                // Give the GUI a second to initialise the pipe:
+                Thread.Sleep(1000);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Failed to start GUI: {ex.Message}");
+                Environment.Exit(1);
+            }
+
+        }
+    }
+}
---
+++ MonSwtExe/ExeProgram.cs
@@ -0,0 +1,63 @@
+﻿using Serilog;
+using System.Diagnostics;
+using System.IO.Pipes;
+
+namespace MonSwtExe;
+
+class ExeProgram
+{
+    const string PipeName = "MonSwt.Pipe";
+    private static readonly DateTime _startTime = DateTime.Now;
+
+    private static void Main(string[] args)
+    {
+        try
+        {
+            var dir = @"c:\temp\logging";
+            Directory.CreateDirectory(dir);
+            var logFile = Path.Combine(dir, "MonSwt.txt");
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .WriteTo.File(logFile, rollingInterval: RollingInterval.Day)
+                .CreateLogger();
+            Log.Information("Log started");
+            Pipe();
+        }
+        catch (Exception ex)
+        {
+            var fullname = System.Reflection.Assembly.GetEntryAssembly().Location;
+            var progname = Path.GetFileNameWithoutExtension(fullname);
+            Console.Error.WriteLine($"{progname} Error: {ex.Message}");
+        }
+    }
+
+    private static void Pipe()
+    {
+        Log.Information("Starting Pipe");
+        while (true)
+        {
+            using (var server = new NamedPipeServerStream(PipeName, PipeDirection.InOut))
+            {
+                server.WaitForConnection();
+                Console.WriteLine("Connection");
+
+                using (var reader = new StreamReader(server))
+                using (var writer = new StreamWriter(server) { AutoFlush = true })
+                {
+                    string line = reader.ReadLine();
+                    if (line == "basic poll")
+                    {
+                        var uptime = (int)(DateTime.Now - _startTime).TotalSeconds;
+                        var pid = Process.GetCurrentProcess().Id;
+                        writer.WriteLine($"Running for {uptime} seconds with PID {pid}");
+                    }
+                    else
+                    {
+                        writer.WriteLine("Unknown command");
+                    }
+                }
+            }
+        }
+
+    }
+}
